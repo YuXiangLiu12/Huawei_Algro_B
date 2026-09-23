d1cdc324050aaf47f73cb5da0cf3aab8eb3947fa
LYX <18852960727@163.com>
2026-09-23 17:42:09 +0800

    删除result文件

--- results/q1_v05/source_before/q1_initial.m
+++
@@ -1,6 +0,0 @@
-function y=q1_initial(d,c,g,ix)
-y=zeros(ix.n,1); y(ix.T)=d.T0;
-y(ix.qH)=g.eps(g.anode)*c.p/(c.R*d.T0);
-y(ix.qO)=g.eps(g.cathode)*c.yO*c.p/(c.R*d.T0);
-y(ix.bn)=g.omega(g.ion)*c.Cm*3;
-end
--- results/q1_v05/source_before/q1_rhs.m
+++
@@ -1,3 +0,0 @@
-function dy=q1_rhs(t,y,d,c,g,ix)
-[dy,~]=q1_rhs_diagnostic(t,y,d,c,g,ix);
-end
